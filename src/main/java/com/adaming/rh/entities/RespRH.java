fbcf486d0810d311b14ce640610b227d41c1eeb2
cracrarys <sim.lobrot@gmail.com>
2020-02-14 15:22:29 +0100

    blablacar

--- src/main/java/com/adaming/rh/entities/RespRH.java
+++ src/main/java/com/adaming/rh/entities/RespRH.java
@@ -2,6 +2,14 @@ package com.adaming.rh.entities;
 
 public class RespRH {
 
+	public long getIdRespRH() {
+		return idRespRH;
+	}
+
+	public void setIdRespRH(long idRespRH) {
+		this.idRespRH = idRespRH;
+	}
+
 	private long idRespRH;
 	
 }
