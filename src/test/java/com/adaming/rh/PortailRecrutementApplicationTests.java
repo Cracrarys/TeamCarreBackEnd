7e4a8b3c1b2264709e50f000963443de37b0ba0c
cracrarys <sim.lobrot@gmail.com>
2020-02-14 13:03:58 +0100

    initialisation projet

---
+++ src/test/java/com/adaming/rh/PortailRecrutementApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.adaming.rh;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class PortailRecrutementApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
