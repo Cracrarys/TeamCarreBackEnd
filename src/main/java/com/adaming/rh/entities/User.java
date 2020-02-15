c5ab2adf738eb9cb33df800f4f99403772ad8aa1
TruongGiang <t.nguyenpham@icloud.com>
2020-02-15 09:39:28 +0100

    add entities dao

---
+++ src/main/java/com/adaming/rh/entities/User.java
@@ -0,0 +1,85 @@
+package com.adaming.rh.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+	@Id
+	private long idUser;
+	@Column
+	private String login;
+	@Column
+	private String password;
+	@Column
+	private boolean activated;
+	@OneToMany(mappedBy = "user")
+	private List<Role> roles = new ArrayList<>();
+
+	public long getIdUser() {
+		return idUser;
+	}
+
+	public void setIdUser(long idUser) {
+		this.idUser = idUser;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public boolean isActivated() {
+		return activated;
+	}
+
+	public void setActivated(boolean activated) {
+		this.activated = activated;
+	}
+
+	public List<Role> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(List<Role> roles) {
+		this.roles = roles;
+	}
+
+	public User(long idUser, String login, String password, boolean activated) {
+		super();
+		this.idUser = idUser;
+		this.login = login;
+		this.password = password;
+		this.activated = activated;
+	}
+
+	public User() {
+		super();
+	}
+
+	@Override
+	public String toString() {
+		return "User [idUser=" + idUser + ", login=" + login + ", password=" + password + ", activated=" + activated
+				+ ", roles=" + roles + "]";
+	}
+
+}
