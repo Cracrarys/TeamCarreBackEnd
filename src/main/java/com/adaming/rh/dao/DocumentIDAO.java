698ab03277e4cf564d4ca6629da74e7dec3fb1b6
Cracrarys <sim.lobrot@gmail.com>
2020-02-18 18:04:36 +0100

    tt

--- src/main/java/com/adaming/rh/dao/DocumentIDAO.java
+++
@@ -1,9 +0,0 @@
-package com.adaming.rh.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.adaming.rh.entities.DocumentRH;
-
-@Repository
-public interface DocumentIDAO extends JpaRepository<DocumentRH, Long> {
-}
